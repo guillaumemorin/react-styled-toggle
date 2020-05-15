bbb15c0f04ba4bd033ce5068fce2027216cfca1d
Guillaume Morin <gmorin.pro@gmail.com>
2020-05-15 13:07:36 +0200

    update build steps

--- index.test.js
+++ index.test.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import Toggle from './src';
-import renderer from 'react-test-renderer';
+import React from 'react'
+import Toggle from './src'
+import renderer from 'react-test-renderer'
 
 it('Standard output', () => {
-    const tree = renderer.create(<Toggle />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
+    const tree = renderer.create(<Toggle />).toJSON()
+    expect(tree).toMatchSnapshot()
+})
 
 it('Disabled status', () => {
-    const tree = renderer.create(<Toggle disabled={true} />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
+    const tree = renderer.create(<Toggle disabled={true} />).toJSON()
+    expect(tree).toMatchSnapshot()
+})
 
 it('Checked status', () => {
-    const tree = renderer.create(<Toggle checked={true} />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
+    const tree = renderer.create(<Toggle checked={true} />).toJSON()
+    expect(tree).toMatchSnapshot()
+})
 
 it('Display right label', () => {
-    const tree = renderer.create(<Toggle labelRight={'Label'} />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
+    const tree = renderer.create(<Toggle labelRight={'Label'} />).toJSON()
+    expect(tree).toMatchSnapshot()
+})
 
 it('Display left label', () => {
-    const tree = renderer.create(<Toggle labelLeft={'Label'} />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
+    const tree = renderer.create(<Toggle labelLeft={'Label'} />).toJSON()
+    expect(tree).toMatchSnapshot()
+})
 
 it('Display left label', () => {
-    const tree = renderer.create(<Toggle labelLeft={'Label'} />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
+    const tree = renderer.create(<Toggle labelLeft={'Label'} />).toJSON()
+    expect(tree).toMatchSnapshot()
+})
 
 it('Display custom background colors', () => {
     const tree = renderer
@@ -40,16 +40,16 @@ it('Display custom background colors', () => {
                 backgroundColorUnchecked={'aquamarine'}
             />
         )
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-});
+        .toJSON()
+    expect(tree).toMatchSnapshot()
+})
 
 it('Set input name and value', () => {
     const tree = renderer
         .create(<Toggle name={'toggle'} value={'toggleValue'} />)
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-});
+        .toJSON()
+    expect(tree).toMatchSnapshot()
+})
 
 it('Display custom sizes', () => {
     const tree = renderer
@@ -62,6 +62,6 @@ it('Display custom sizes', () => {
                 translate={19}
             />
         )
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-});
+        .toJSON()
+    expect(tree).toMatchSnapshot()
+})
